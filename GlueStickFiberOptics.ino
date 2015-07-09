f6d98ba4e562dd22d2d281164b36d55857282579
Jonah Duckles <jonah@duckles.org>
2015-07-09 11:05:40 -0500

    initial commit

---
+++ GlueStickFiberOptics.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(0,HIGH);
+  delay(5000);
+  digitalWrite(0,LOW);
+}
