4cfda7aa6dad25923c695c5e6c1ab9d6228f85b2
Jonah Duckles <jonah@duckles.org>
2015-07-09 15:56:36 -0500

    Uploading my running average

---
+++ OpticalTXRX/OpticalTXRX.ino
@@ -0,0 +1,31 @@
+const int TX = 10;
+const int RX = A1;
+float RXmean = 0;
+float counter = 0;
+int cycles = 10000;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int RXvalue = analogRead(RX);
+  if (counter == cycles) { 
+    RXmean = (RXmean + RXvalue) / 2; 
+    Serial.println(String(RXmean) + " " + String(RXvalue));
+    counter = 0;
+  }
+      
+  if (RXvalue > RXmean + 50) {
+    digitalWrite(TX, HIGH);
+  }
+  else {
+    digitalWrite(TX, LOW);
+  }
+  counter++;
+
+}
